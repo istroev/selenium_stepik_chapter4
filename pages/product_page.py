1c0ac169a0cf6e1ccf3a33ec73f4d54efee6cf63
Ivan <i.stroev@pentestit.ru>
2019-10-23 22:56:58 +0300

    add product page parametrize

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,9 +10,9 @@ class ProductPage(BasePage):
     def check_added_item(self):
         item_name = self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
         basket_name = self.browser.find_element(*ProductPageLocators.ADDED_ALERT).text
-        assert item_name == basket_name, "Basket item name not equal to item name"
+        assert item_name == basket_name, f"Basket item name {basket_name} not equal to item name {item_name} on page {self.url}"
 
     def check_basket_price(self):
         price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
         basket_price = self.browser.find_element(*ProductPageLocators.PRICE_ALERT).text
-        assert price == basket_price, f"Basket price {basket_price} is not equal to item {price} price"
+        assert price == basket_price, f"Basket price {basket_price} is not equal to item {price} price on page {self.url}"
